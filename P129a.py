d47fc6149aa503aa97ec79adadc82e86a5f6d2a4
riya-the-coder <58181072+riya-the-coder@users.noreply.github.com>
2021-06-22 15:01:29 +0530

    Add files via upload

---
+++ P129a.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df=pd.read_csv("dwarf_stars.csv")
+df.head()
+df.columns
+df=df.dropna()
+df.dtypes
+df["Radius"]=0.102763*df["Radius"]
+df["Mass"]=df['Mass'].apply(lambda x: x.replace('$', '').replace(',', '')).astype('float')
+df["Mass"]=0.000954588* df["Mass"]
+df.head()
+df.columns
+df.drop(['Unnamed: 0'],axis=1,inplace=True)
+df.reset_index(drop=True,inplace=True)
+df.to_csv("unitconvertedstars.csv")
